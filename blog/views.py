9e8dfebb098844b983cc5a23dc7eb6ce7540a115
itay regev <30416897+itay1regev@users.noreply.github.com>
2019-04-25 15:18:38 +0300

    models object-html templates – and view connector

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
+
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
@@ -9,7 +13,17 @@ from django.shortcuts import render
 # def post_list(request):
 #     return render(request, 'templates/blog/post_list.html', {})
 
+
+
+
+"""
 # it gets to the templates auto' way.
+# request -  everything we receive from the user via the Internet 
+#  giving the template file ('blog/post_list.html'). The last parameter, {}, is a place in which we can add some things
+#  for the template to use. We need to give them names (we will stick to 'posts' right now). :) It should look like
+#  this: {'posts': posts}. Please note that the part before : is a string; you need to wrap it with quotes: ''
+"""
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
